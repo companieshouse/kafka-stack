089e9ffe1d89a4e177fce55723747d5931f639ec
Jimmy <jtaylor@companieshouse.gov.uk>
2022-05-26 07:34:46 +0100

    Improve bootstrap zookeeper selection

--- groups/kafka/locals.tf
+++ groups/kafka/locals.tf
@@ -3,7 +3,6 @@ locals {
   account_ids = data.vault_generic_secret.account_ids.data
   applications_subnets = values(data.aws_subnet.applications)
   automation_subnets = values(data.aws_subnet.automation)
-  zookeeper_hostnames = sort(values(data.aws_instance.zookeepers).*.tags.HostName)
   placement_subnets = values(data.aws_subnet.placement)
   placement_subnets_by_availability_zone = {
     for subnet in data.aws_subnet.placement : subnet.availability_zone => subnet
@@ -19,6 +18,13 @@ locals {
 
   vpc_id = data.aws_vpc.placement.id
 
+  zookeepers_by_availability_zone = {
+    for availability_zone in toset(values(data.aws_instance.zookeepers).*.availability_zone) : availability_zone => sort([
+      for instance in values(data.aws_instance.zookeepers) : instance.tags.HostName
+        if instance.availability_zone == availability_zone
+    ])
+  }
+
   # ----------------------------------------------------------------------------
 
   ami_owner_id = local.secrets.kafka_ami_owner_id
@@ -26,9 +32,14 @@ locals {
   applications_vpc_pattern = local.secrets.applications_vpc_pattern
   automation_subnet_pattern = local.secrets.automation_subnet_pattern
   automation_vpc_pattern = local.secrets.automation_vpc_pattern
+
+  bootstrap_zookeepers = [
+    for instances in values(local.zookeepers_by_availability_zone) : instances[0]
+      if length(instances) > 0
+  ]
+
   dns_server_ip = local.secrets.dns_server_ip
   dns_zone_name = local.secrets.dns_zone_name
-  kafka_zookeeper_connect_string = join(",", formatlist("%s:2181", local.zookeeper_hostnames))
   ns_update_key_content = local.secrets.ns_update_key_content
   placement_subnet_pattern = local.secrets.placement_subnet_pattern
   placement_vpc_pattern = local.secrets.placement_vpc_pattern
@@ -44,6 +55,8 @@ locals {
     local.automation_subnets.*.availability_zone,
     local.automation_subnets.*.cidr_block
   ))
+
+  kafka_zookeeper_connect_string = join(",", formatlist("%s:2181", local.bootstrap_zookeepers))
 
   placement_subnet_cidrs = values(zipmap(
     local.placement_subnets.*.availability_zone,
