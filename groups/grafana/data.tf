66f86798f3c76119875efd7ca13af5b615d9ea22
Jimmy <jtaylor@companieshouse.gov.uk>
2022-05-31 07:42:12 +0100

    Replace grafana ssh_keyname variable with dynamic lookup

--- groups/grafana/data.tf
+++ groups/grafana/data.tf
@@ -2,6 +2,18 @@ data "aws_ec2_managed_prefix_list" "administration" {
   name = "administration-cidr-ranges"
 }
 
+data "aws_key_pair" "kafka_stack" {
+  filter {
+    name   = "tag:Environment"
+    values = [var.environment]
+  }
+
+  filter {
+    name   = "tag:Service"
+    values = [var.service]
+  }
+}
+
 data "vault_generic_secret" "account_ids" {
   path = "aws-accounts/account-ids"
 }
